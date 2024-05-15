ba19b7528821fac2a2c4450f06ecfad9684eafc5
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-15 19:39:15 +0300

    fixing the square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 class square():
-    
+    '''A square class'''
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        '''initializing the square '''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +16,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        '''per of the square'''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        '''beautifying the output'''
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
